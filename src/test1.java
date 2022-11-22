0debc77483294bfb1cb5a8a1c69771123f7474eb
abkas <abkas@MSI>
2022-11-22 22:11:39 +0530

    class 2 added

--- src/test1.java
+++ src/test1.java
@@ -2,7 +2,7 @@
 public class test1 {
 	
 	
-	private void studytry() {
+	public void studytry() {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello world");
---
+++ src/test2.java
@@ -0,0 +1,10 @@
+
+public class test2 {
+	
+	public void testing() {
+		// TODO Auto-generated method stub
+System.out.println("class2");
+	}
+	
+
+}
